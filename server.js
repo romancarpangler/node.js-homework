f46cda98979b0db2d32268e9c4e245a4b52d0626
romancarpangler <korzunroman56@gmail.com>
2023-05-30 16:49:30 +0300

    made functions for working with the database

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 
-
 const { host } = process.env;
 mongoose
   .connect(host)
   .then(() => {
     app.listen(3000, () => {
-      console.log("Server running. Use our API on port: 3000");
+      console.log("Database connection successful");
     });
   })
   .catch((err) => {
