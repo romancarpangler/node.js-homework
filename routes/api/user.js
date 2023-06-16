c2b2f094efcf60727ffe67ea0b4eb042d00a7c59
Roman <korzunroman56@gmail.com>
2023-06-16 12:45:15 +0300

    hw06

--- routes/api/user.js
+++ routes/api/user.js
@@ -5,10 +5,15 @@ const validUser = require("../../middlewares/validateUser");
 const user = require("../../controlers/user");
 const authenticate = require("../../middlewares/authenticate");
 const upload = require("../../middlewares/upload");
+const { verify } = require("../../middlewares/repeadVerifay");
 
 const vu = validUser.validateUser;
 
 router.post("/register", vu, user.registerUser);
+
+router.get("/verify/:verificationToken", user.verifyUser);
+
+router.post("/verify", verify, user.repeatedVerify);
 
 router.post("/login", vu, user.loginUser);
 
--- shema/user.js
+++ shema/user.js
@@ -16,6 +16,14 @@ const userValidate = new mongoose.Schema(
       enum: ["starter", "pro", "business"],
       default: "starter",
     },
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+      required: [true, "Verify token is required"],
+    },
     token: String,
     avatarURL: String,
   },
